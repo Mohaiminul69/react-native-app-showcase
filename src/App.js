e5edfa6b2fd5e83289a8e02e6245f87e40d51bf7
mohaiminul69 <mohaiminul69@gmail.com>
2022-11-05 16:29:11 +0600

    react native app showcase done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { SectionWrapper } from "./components";
+import { Download, Features, SectionWrapper } from "./components";
 import assets from "./assets";
+import styles from "./styles/Global";
 
 const App = () => {
   return (
@@ -11,6 +12,34 @@ const App = () => {
         mockupImg={assets.homeHero}
         banner="banner"
       />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Site managed by{" "}
+          <span className="bold">Mohaiminul Islam Shishir.</span> Copyright ©
+          <span className="bold">Shishir</span> 2022. All rights reserved.
+        </p>
+      </div>
     </>
   );
 };
